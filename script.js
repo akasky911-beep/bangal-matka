f63d9405f414b6051089eda9f2cb14e91750aec9
akasky911-beep <akashky0777official@gmail.com>
2026-09-20 06:13:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const SUPABASE_URL = "https://isblbvsoshxxwoplptor.supabase.co";
-
 const SUPABASE_KEY = "sb_publishable_Bj0alx7zydRokk4Kt50dDQ_6OGdLZAT";
-
 const client = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
